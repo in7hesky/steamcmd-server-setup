e592ab8b1e38432860890cd1261f2c24d0d74fd1
in7hesky <anton.maiko.dev@gmail.com>
2024-12-07 03:53:46 +0300

    add deps install step

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+echo "Installing dependencies"
+
+apt update && apt install software-properties-common
+apt-add-repository non-free && dpkg --add-architecture i386
+apt update && sudo apt install steamcmd
+
 echo "Setting admin user"
+
 read -p "Enter username: " username
-
 useradd -m $username
-
 read -sp "Enter password: " password
-
 passwd $username << EOF
 $password
 $password
